acd56b2623024f22835598a53e44485dae981414
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-02-26 16:10:00 -0600

    Slowing and Better jumping

--- Meatball.gd
+++ Meatball.gd
@@ -6,16 +6,16 @@ export(float) var jumpHeight = 20
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	velocity.y += gravity
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			inputVector.y -= jumpHeight
-	else:
-		inputVector.y = gravity
+	if is_on_floor() && Input.is_action_just_pressed("ui_up"):
+		inputVector.y -= jumpHeight
 	
 	if inputVector != Vector2.ZERO:
-		velocity = inputVector * speed
+		velocity += inputVector * speed
+	else:
+		velocity.x = lerp(0,velocity.x,.9)
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
