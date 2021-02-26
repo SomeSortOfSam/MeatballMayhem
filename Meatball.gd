b452513c65d663b27cf30bb5150fdc94cdd8e2b2
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-02-26 15:55:17 -0600

    Gravity and Jumping
    Jumping needs some work to allow sideways

--- Meatball.gd
+++ Meatball.gd
@@ -1,16 +1,21 @@
 extends KinematicBody2D
 
-export(float) var speed = 10
+export(float) var speed = 100
+export(float) var gravity = 10
+export(float) var jumpHeight = 20
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			inputVector.y -= jumpHeight
+	else:
+		inputVector.y = gravity
 	
 	if inputVector != Vector2.ZERO:
 		velocity = inputVector * speed
-	else:
-		velocity = Vector2.ZERO
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
