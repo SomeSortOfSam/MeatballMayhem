7d4ff6286fd7d693cd0f0850f3cca3e43c2448a0
Hederarch <66890769+Hederarch@users.noreply.github.com>
2021-02-27 17:41:36 -0600

    yay i coded :)

--- Tiles/KillNode.gd
+++ Tiles/KillNode.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 onready var platform = $StaticBody2D/CollisionShape2D
 onready var sprite = $Sprite
+onready var hurtbox = $CollisionShape2D
 
 func _on_Kill_body_entered(body):
 	body.kill()
-	platform.set_deferred("disabled",false)
-	sprite.set_self_modulate(Color.green)
+	hurtbox.set_deferred("disabled",false)
+	platform.shape.set_extents(Vector2(32,32))
+	platform.set_position(Vector2(32,32))
