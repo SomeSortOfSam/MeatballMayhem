fe94e784a2f33dd1bc40f2b52e42a030b50f0f6e
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-02-27 18:11:03 -0600

    Spike Fix

--- Tiles/Spikes.gd
+++ Tiles/Spikes.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 onready var platform = $StaticBody2D/CollisionShape2D
 onready var sprite = $Sprite
-onready var hurtbox = $CollisionShape2D
+onready var hurtbox = $HurtBox
 
 func _on_Kill_body_entered(body):
 	body.kill()
 	hurtbox.set_deferred("disabled",false)
 	platform.shape.set_extents(Vector2(32,32))
-	platform.set_position(Vector2(32,32))
+	platform.set_position(Vector2.ZERO)
+	sprite.frame += 1
+	if body.cooked:
+		sprite.frame += 1
