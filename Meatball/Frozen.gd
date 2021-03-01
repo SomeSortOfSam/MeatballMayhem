9501fafdea2e4e10027648756d12759aa5195b5b
Portal <57550537+Theportalmasters@users.noreply.github.com>
2021-02-28 21:59:05 -0600

    Frozen fix

--- Meatball/Frozen.gd
+++ Meatball/Frozen.gd
@@ -1,7 +1,13 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 onready var sprite = $Sprite
+onready var left = $Left
+onready var right= $Right
 
+export(float) var acceleration = 100
+export(float) var maxSpeed = 300
+export(float) var gravity = 9.8
+var velocity = Vector2.ZERO
 var cooked = false
 
 var paused = false
@@ -17,7 +23,21 @@ func _process(_delta):
 			sprite.playing = true
 
 func _physics_process(_delta):
-	pass
+	var inputVector = Vector2.ZERO
+	if left.is_colliding():
+		inputVector.x -= acceleration
+	if right.is_colliding():
+		inputVector.x += acceleration
+	if is_on_floor():
+		rotation = get_floor_normal().angle() + PI/2
+	else:
+		rotation = 0
+		inputVector.y += gravity
+	
+	velocity += inputVector
+	velocity.x = lerp(clamp(velocity.x,-maxSpeed,maxSpeed),0,.1)
+	velocity = move_and_slide(velocity,Vector2.UP)
+	
 
 func animate(animation : String,endFunction = ""):
 	if endFunction != "":
--- Meatball/Meatball.gd
+++ Meatball/Meatball.gd
@@ -22,7 +22,7 @@ var paused = false
 
 export(bool) var hasDoneDeluxeRespawn = true
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		paused = !paused
 		if paused:
